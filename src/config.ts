7eaecb9c32bc2a54554b5a1550680007a1a749e2
msjulyone <msjulyone@gmail.com>
2021-01-19 20:08:47 +0900

    Fix: mockData

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
 export const API = "https://localhost:3000";
+export const boardAPI = "http://10.58.2.55:3000/data/PostListData.json";
--- src/pages/PostList.tsx
+++ src/pages/PostList.tsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { boardAPI } from "../config";
+import styled from "styled-components";
 
 export default function PostList() {
-  return <div>PostList</div>;
+  interface PostListData {
+    id: number;
+    title: string;
+    writer: string;
+    registrationData: string;
+    comments: number;
+  }
+  const [boardDatas, setBoardDatas] = useState<PostListData[]>([]);
+
+  useEffect(() => {
+    axios.get(boardAPI).then((res) => setBoardDatas(res.data));
+  });
+  return (
+    <div>
+      <ul>
+        <Title>
+          <li>제목</li>
+          <li>작성자</li>
+          <li>등록일</li>
+          <li>댓글</li>
+        </Title>
+        {boardDatas?.slice(0, 5).map((list) => {
+          return (
+            <Content>
+              <li>{list.title}</li>
+              <li>{list.writer}</li>
+              <li>{list.registrationData}</li>
+              <li>{list.comments}</li>
+            </Content>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
+
+const Title = styled.div`
+  display: flex;
+`;
+
+const Content = styled.div`
+  display: flex;
+`;
