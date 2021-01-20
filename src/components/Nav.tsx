65a253e89accecf3743a5da3d19987c03dba5ae1
Jean Baek <jeanbaek710@gmail.com>
2021-01-20 23:54:57 +0900

    Add: 회원가입 및 로그인 레이아웃 1차 완료

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { mixin } from "../styles";
+import { Signup, Login } from "./index";
 
 interface IProps {
   weight: {
@@ -78,7 +79,7 @@ export default function Nav({
       >
         <ModalInner tabIndex={0}>
           <CloseBtn onClick={closeModal}>삭제</CloseBtn>
-          회원가입 or 로그인 모달창
+          {modalActive.signup || !modalActive.login ? <Signup /> : <Login />}
         </ModalInner>
       </ModalWrapper>
     </Container>
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,5 +1,90 @@
 import React from "react";
+import styled from "styled-components";
+import { mixin } from "../styles";
+
+interface StateForStyle {
+  short?: boolean;
+  needSpace?: boolean;
+}
 
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <Container>
+      <Title>회원가입</Title>
+      <InfoSection action="" method="POST">
+        <BlockWrapper>
+          <Subject short={true}>기수</Subject>
+          <Input type="text" name="order" short={true}></Input>
+          <Subject short={true} needSpace={true}>
+            이름
+          </Subject>
+          <Input type="text" name="fullName" short={true}></Input>
+        </BlockWrapper>
+        <BlockWrapper>
+          <Subject>이메일</Subject>
+          <Input type="email" name="email"></Input>
+        </BlockWrapper>
+        <BlockWrapper>
+          <Subject>비밀번호</Subject>
+          <Input type="text" name="password"></Input>
+        </BlockWrapper>
+        <BlockWrapper>
+          <Subject>비밀번호 확인</Subject>
+          <Input type="text" name="rePassword"></Input>
+        </BlockWrapper>
+        <Button type="submit" value="회원가입"></Button>
+      </InfoSection>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  margin: 2em 1.5em;
+`;
+
+const Title = styled.h1`
+  height: 1em;
+  text-align: center;
+  font-size: 6em;
+  font-weight: 900;
+  margin-bottom: 0.5em;
+  letter-spacing: 0.2em;
+`;
+
+const InfoSection = styled.form`
+  margin: 0 2.5em;
+  height: 20em;
+`;
+
+const BlockWrapper = styled.div`
+  margin: 2em 0;
+  height: 2.5em;
+  justify-content: space-between;
+  align-items: center;
+  display: flex;
+`;
+
+const Subject = styled.div<StateForStyle>`
+  text-align: center;
+  width: 3.5em;
+  margin-right: 1.5em;
+  margin-left: ${({ needSpace }) => (needSpace ? "1.5em" : "0")};
+`;
+
+const Input = styled.input<StateForStyle>`
+  width: ${({ short }) => (short ? "10.5em" : "30em")};
+  height: 4em;
+`;
+
+const Button = styled.input`
+  margin-top: 2em;
+  width: 28.6em;
+  font-size: 1em;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 4em;
+  text-align: center;
+  background-color: white;
+`;
