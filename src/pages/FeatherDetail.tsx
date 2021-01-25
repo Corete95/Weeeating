3c39dd3e29e744b460afb6d3f42bd7888ae839ef
Corete95 <fanthell95@gmail.com>
2021-01-25 17:53:08 +0900

    ADD:깃털푸드 페이지 레이아웃 작업중

--- src/pages/FeatherDetail.tsx
+++ src/pages/FeatherDetail.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { API1 } from "../config";
+import StoreCard2 from "./childComponents/StoreCard2";
+import axios from "axios";
+import "./FeatherDetail.scss";
 
 export default function FeatherDetail() {
-  return <div>깃털푸드 디테일</div>;
+  const [featherFood, setFeatherFood] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API1}`).then((response) => {
+      setFeatherFood(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="feather">
+        <div className="logoBox">
+          <div className="logoBox1">
+            <p>GIT털 푸드</p>
+          </div>
+        </div>
+        <div className="featherFood">
+          {featherFood?.map((feather: any) => {
+            return (
+              <StoreCard2
+                image={feather.image}
+                title={feather.title}
+                heart={feather.heart}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
