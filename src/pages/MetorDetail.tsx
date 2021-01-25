ea8ee3195337c1fcd06e0980b0fad0aa13f9484b
Jean Baek <jeanbaek710@gmail.com>
2021-01-25 22:38:03 +0900

    Add: 슬라이드 및 모든 기능 제외, 레이아웃 1차 완료

--- src/pages/MetorDetail.tsx
+++ src/pages/MetorDetail.tsx
@@ -15,7 +15,7 @@ export default function MetorDetail() {
   useEffect(() => {
     axios
       .get(`${LOCALAPI}/data/mentor.json`)
-      .then((res: any) => setMentorInfo(res))
+      .then((res: any) => setMentorInfo(res.data))
       .catch((err: any) => console.log("Catched Errors!! >>>", err));
   }, []);
 
@@ -39,34 +39,32 @@ export default function MetorDetail() {
             </div>
           </Arrow>
         </Header>
-        <Body>
-          <CardWrapper>
-            {mentorInfo.length &&
-              mentorInfo.map(
-                ({ id, image, name, position, store_id, store, desc }: any) => (
-                  <CardEach>
-                    <Profile>
-                      <img src={image} />
-                      <div className="name">{name}</div>
-                      <div className="position">{position}</div>
-                    </Profile>
-                    <Desc>
-                      <div className="storeHeader">
-                        <span className="decoTitle">"</span>
-                        <span className="storeTitle">{store}</span>
-                        <span className="decoTitle">"</span>
-                      </div>
-                      <div className="storeDesc">{desc}</div>
-                    </Desc>
-                    <MoreBtn>
-                      <span className="seeStoreInfo">맛집 구경하기</span>
-                      <IoIosAddCircle />
-                    </MoreBtn>
-                  </CardEach>
-                )
-              )}
-          </CardWrapper>
-        </Body>
+        <CardWrapper>
+          {mentorInfo.length &&
+            mentorInfo.map(
+              ({ id, image, name, position, store_id, store, desc }: any) => (
+                <CardEach id={id}>
+                  <Profile>
+                    <img src={image} />
+                    <div className="name">{name}</div>
+                    <div className="position">{position}</div>
+                  </Profile>
+                  <Desc>
+                    <div className="storeHeader">
+                      <span className="decoTitle">" </span>
+                      <span className="storeTitle">{store}</span>
+                      <span className="decoTitle"> "</span>
+                    </div>
+                    <div className="storeDesc">{desc}</div>
+                  </Desc>
+                  <MoreBtn>
+                    <span className="seeStoreInfo">맛집 구경하기</span>
+                    <IoIosAddCircle className="btnIcon" />
+                  </MoreBtn>
+                </CardEach>
+              )
+            )}
+        </CardWrapper>
       </Container>
     </Wrapper>
   );
@@ -76,10 +74,10 @@ const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: ${({ theme }) => theme.mentorBack};
+  line-height: 1.3;
 `;
 
 const Container = styled.div`
-  border: 1px solid black;
   margin: 5rem auto;
   padding-top: 3rem;
   width: 65rem;
@@ -89,11 +87,13 @@ const Header = styled.section`
   display: flex;
   justify-content: space-between;
   align-items: flex-end;
+  margin-bottom: 3.9rem;
 `;
 
 const Title = styled.h2`
-  width: 37.8rem;
+  width: 53.2rem;
   font-size: 2.5rem;
+  letter-spacing: 0.4rem;
 `;
 
 const Line = styled.div`
@@ -131,25 +131,69 @@ const Arrow = styled.div`
   }
 `;
 
-const Body = styled.section``;
-
 const CardWrapper = styled.div``;
 
 const CardEach = styled.div`
+  position: relative;
   float: left;
   width: 33.33%;
-  margin-right: 1rem;
+  height: 40rem;
+  padding-right: 1rem;
 `;
 
-const Profile = styled.section``;
+const Profile = styled.section`
+  padding: 1.25rem;
+
+  img {
+    border-radius: 50%;
+    width: 15rem;
+    height: 15rem;
+  }
+
+  .name {
+    font-size: 1.3rem;
+    font-weight: 900;
+    line-height: 1.14;
+    margin-top: 1.94rem;
+  }
+
+  .position {
+    margin-top: 0.6rem;
+    font-size: 1rem;
+    line-height: 1.07;
+  }
+`;
 
 const Desc = styled.section`
+  line-break: anywhere;
+  padding: 1.25rem 1.13rem;
+  line-height: 1.88;
+  margin-bottom: 2.25rem;
+
   .storeHeader {
     .decoTitle {
     }
     .storeTitle {
+      font-weight: 900;
+      font-size: 1.15rem;
     }
   }
 `;
 
-const MoreBtn = styled.div``;
+const MoreBtn = styled.span`
+  cursor: pointer;
+  font-size: 1.2rem;
+  margin: 2.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 7rem;
+  height: 1.5rem;
+  position: absolute;
+  bottom: 0;
+  right: 0rem;
+
+  .btnIcon {
+    font-size: 1.3rem;
+  }
+`;
