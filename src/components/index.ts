f35e99ec9e4e56c628e02b71bea8ed596fe30aba
Jean Baek <jeanbaek710@gmail.com>
2021-01-28 12:28:40 +0900

    Add: 회원가입 시 필요한 로직 구성(입력값 저장, API 통신, 유효성검사

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ export { default as Footer } from "./Footer";
 export { default as Login } from "./Login";
 export { default as Nav } from "./Nav";
 export { default as Signup } from "./Signup";
+export { default as SignupForm } from "./SignupForm";
 export { default as GoogleLoginComponent } from "./GoogleLoginComponent";
 export { default as PeoplePick } from "./PeoplePick";
