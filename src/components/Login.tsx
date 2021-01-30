ff8573f64175718cb02ea0927b92978ebebe038c
Jean Baek <jeanbaek710@gmail.com>
2021-01-30 17:18:21 +0900

    Modify: register API 통신 로직 endpoint 변경, params를 body로 수정

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -25,27 +25,27 @@ export default function Login() {
   };
 
   const submitLogin = () => {
-    //   const params = {
-    //     email: user.email,
-    //     password: user.password
-    //   };
-    //   axios
-    //     .post(`${BEAPI}`, JSON.stringify(params))
-    //     .then((res) => {
-    //       if (res.data.success === "SUCCESS") {
-    //         if (window.localStorage.getItem("token")) {
-    //           localStorage.token = res.data.token;
-    //           localStorage.isAuthenticated = true;
-    //           window.location.reload();
-    //         } else {
-    //           window.localStorage.setItem("token", res.data.token);
-    //           window.location.reload();
-    //         }
-    //       } else {
-    //         alert("로그인이 완료되지 않았습니다.");
-    //       }
-    //     })
-    //     .catch((err) => console.log("로그인 통신이 원활하지 않습니다.", err));
+    const data = {
+      email: user.email,
+      password: user.password
+    };
+    axios
+      .post(`${BEAPI}/user/login`, { data })
+      .then((res) => {
+        if (res.data.success === "SUCCESS") {
+          if (window.localStorage.getItem("token")) {
+            localStorage.token = res.data.token;
+            localStorage.isAuthenticated = true;
+            window.location.reload();
+          } else {
+            window.localStorage.setItem("token", res.data.token);
+            window.location.reload();
+          }
+        } else {
+          alert("로그인이 완료되지 않았습니다.");
+        }
+      })
+      .catch((err) => console.log("로그인 통신이 원활하지 않습니다.", err));
   };
 
   const props = { handleChange, submitLogin };
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -22,29 +22,29 @@ export default function Signup() {
 
   const submitSingup = () => {
     if (user.password === user.repassword || !user.password) {
-      //   const params = {
-      //     number: user.number,
-      //     name: user.userName,
-      //     email: user.email,
-      //     password: user.password
-      //   };
-      //   axios
-      //     .post(`${BEAPI}`, JSON.stringify(params))
-      //     .then((res) => {
-      //       if (res.data.success === "SUCCESS") {
-      //         if (window.localStorage.getItem("token")) {
-      //           localStorage.token = res.data.token;
-      //           localStorage.isAuthenticated = true;
-      //           window.location.reload();
-      //         } else {
-      //           window.localStorage.setItem("token", res.data.token);
-      //           window.location.reload();
-      //         }
-      //       } else {
-      //         alert("회원가입이 완료되지 않았습니다.");
-      //       }
-      //     })
-      //     .catch((err) => console.log("회원가입 통신이 원활하지 않습니다.", err));
+      const data = {
+        number: user.number,
+        name: user.userName,
+        email: user.email,
+        password: user.password
+      };
+      axios
+        .post(`${BEAPI}/user/signup`, { data })
+        .then((res) => {
+          if (res.data.success === "SUCCESS") {
+            if (window.localStorage.getItem("token")) {
+              localStorage.token = res.data.token;
+              localStorage.isAuthenticated = true;
+              window.location.reload();
+            } else {
+              window.localStorage.setItem("token", res.data.token);
+              window.location.reload();
+            }
+          } else {
+            alert("회원가입이 완료되지 않았습니다.");
+          }
+        })
+        .catch((err) => console.log("회원가입 통신이 원활하지 않습니다.", err));
     } else {
       alert(
         "재입력한 비밀번호가 일치하지 않습니다. 비밀번호를 다시 입력해주세요."
