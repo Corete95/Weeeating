13a7803510a04a94ba375e2101266c7f9f20ed35
Jean Baek <jeanbaek710@gmail.com>
2021-01-31 01:35:30 +0900

    Modify: 서버 IP 주소 변경

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const BEAPI = "http://10.58.0.152:8000";
+export const BEAPI = "http://172.30.1.23:8000";
 export const LOCALAPI = "http://localhost:3000";
 export const API = "http://localhost:3000/data/storeCardData.json";
 export const API1 = "http://localhost:3000/data/alcoholCardData.json";
